22f11657e84bfa2cdd0053e64c1d8daecce8187c
Martin Agnar Dahl <Martin.Agnar.Dahl@norconsult.com>
2022-12-15 19:27:43 +0100

    Add project files.

---
+++ CoursePlatformClient/Server/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using CoursePlatformClient.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoursePlatformClient.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
